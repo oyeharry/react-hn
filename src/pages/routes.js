1e3f0a6a3b13197dc56d05e7f6f081e4734bc53b
Harminder Pal Singh <harminderpal.singh@publicissapient.com>
2020-04-18 18:26:25 +0530

    added runtime caching for remaining routes

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -4,22 +4,26 @@ import Page404 from './404';
 const routes = [
   {
     path: '/news/:pageNum',
+    cache: true,
     component: News,
     getServerSideProps: newsGetServerSideProps,
   },
   {
     path: '/news',
+    cache: true,
     component: News,
     getServerSideProps: newsGetServerSideProps,
   },
   {
     path: '/',
+    cache: true,
     exact: true,
     component: News,
     getServerSideProps: newsGetServerSideProps,
   },
   {
     path: '/*',
+    cache: false,
     component: Page404,
   },
 ];
